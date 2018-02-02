2a920a576aec68759b927fc3c2e98d319197ddf9
Shen Yu <445004152@qq.com>
2018-02-02 15:10:53 +0800

    rsub

--- 041magic_new.py
+++ 041magic_new.py
@@ -1,7 +1,7 @@
-class CapStr(str):
+class CapStr(str):#继承str,str不可修改类型,无法用init修改
     def __new__(cls, string):
-        string = string.upper()
-        return str.__new__(cls, string)
+        string1 = string.upper()#new的时候重写,调用字符串的内置方法
+        return str.__new__(cls, string1)#return一个实例化对象,先把内容变了,再构造一次传回去
 
 a = CapStr("hey, yu!")
 print(a)
---
+++ 043rsub.py
@@ -0,0 +1,6 @@
+class Nint(int):
+    def __rsub__(self, other):
+        return int.__sub__(self, other)
+
+a = Nint(5)
+print(3 - a)
